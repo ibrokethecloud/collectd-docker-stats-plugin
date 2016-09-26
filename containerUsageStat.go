81e5bcf7751e68462082ddd4dbd65dc3580652a6
Gaurav Mehta <gaurav.mehta@iag.com.au>
2016-09-26 17:25:07 +1000

    Added support for Introscope EPAgent

--- containerUsageStat.go
+++ containerUsageStat.go
@@ -17,7 +17,7 @@ import (
 func containerUsageStats (cli *client.Client, containers []types.Container, statsPtr *string,modePtr *string){
 	// Pass the client connection from the main function as don't need to redo the logic to see if we are using unix sockets or tcp connection
 
-	
+
 	var container types.Container
 	// Iterate over all available containers in the map and identify running containers first //
 	for _, container = range containers{
@@ -25,7 +25,7 @@ func containerUsageStats (cli *client.Client, containers []types.Container, stat
 		//tmpContainerArray := strings.Fields(pair)
 		//fmt.Printf("%s - %s - %s \n",container.ID, container.State, container.Status)
 		// Check if container.Status contains the String running and then get stats for that container //
-		// Check for metric type first and then if container is up before proceeding 
+		// Check for metric type first and then if container is up before proceeding
         var containerName string
         for _, containerName = range container.Names{
 
@@ -45,7 +45,7 @@ func containerUsageStats (cli *client.Client, containers []types.Container, stat
 		}
 
 		//fmt.Printf("%f \n", <-channel)
-			
+
 	}
 }
 
@@ -69,18 +69,18 @@ func GenerateCPUStats (cli *client.Client, container_id string, containerName st
 		if systemDelta > 0.0 &&  cpuDelta > 0.0 {
 			cpuPercentage = float64(cpuDelta) / float64(systemDelta) * float64(len(dockerStats.CPUStats.CPUUsage.PercpuUsage)) * 100.0
 		}
-		
 
-			
+
+
 	//fmt.Printf("%s - %f \n",container_id,cpuPercentage)
 	if *modePtr == "collectd"{
 		collectdFormatter(containerName, cpuPercentage, "cpuPercentage")
 		} else if *modePtr == "introscope"{
-			log.Fatal("Feature coming soon")
+			introscopeFormatter(containerName, cpuPercentage, "cpuPercentage")
 		}else {
 			log.Fatal("Logging mode not yet available")
 		}
-	
+
 	//return cpuPercentage
 }
 
@@ -94,7 +94,7 @@ func GenerateMemStats (cli *client.Client, container_id string,containerName str
 		var dockerStats types.Stats
 		err = json.Unmarshal(content, &dockerStats)
 		memPercentage := 0.0
-	
+
 		if dockerStats.MemoryStats.Limit != 0 {
 				memPercentage = float64(dockerStats.MemoryStats.Usage) / float64(dockerStats.MemoryStats.Limit) * 100.0
 			}
@@ -102,11 +102,11 @@ func GenerateMemStats (cli *client.Client, container_id string,containerName str
 		if *modePtr == "collectd"{
 			collectdFormatter(containerName, memPercentage, "memPercentage")
 		} else if *modePtr == "introscope"{
-			log.Fatal("Feature coming soon")
+			introscopeFormatter(containerName, memPercentage, "memPercentage")
 		}else {
 			log.Fatal("Logging mode not yet available")
 		}
-		
+
 	//fmt.Printf("%s - %f \n",container_id,memPercentage)
-	
+
 }
---
+++ introscope-formatter.go
@@ -0,0 +1,38 @@
+// Will be using this to format the output to be compatible with introscope plugin //
+package main
+
+import (
+    "fmt"
+    "strings"
+    "strconv"
+    "net/http"
+    "bytes"
+    "io/ioutil"
+)
+
+
+func introscopeFormatter(containerName string, metricValue float64, metricName string){
+    // Accepts the map built by the earlier step and formats it up //
+    // Getting current Hostname which should be available from collectd plugin //
+    // Sample url for calling epagent curl -H "Content-Type: application/json" --data @epagent_rest_sample.json http://centos64rt2.ca.com:8080/apm/metricFeed
+    containerName = strings.TrimLeft(containerName,"/")
+    stringmetricValue := strconv.FormatFloat(metricValue,'f',0,64)
+    remoteUrl := "http://umr71hm7.auiag.corp:8080/apm/metricFeed"
+
+    metricString := "{\"metrics\":[{type:\"IntCounter\",name:\"DockerStats|" + containerName + ":" + metricName + "\",value:\"" + stringmetricValue + "\"}]}"
+    jsonStr := []byte(metricString)
+    req, err := http.NewRequest("POST", remoteUrl, bytes.NewBuffer(jsonStr))
+    req.Header.Set("Content-Type", "application/json")
+
+    client := &http.Client{}
+    resp, err := client.Do(req)
+    if err != nil{
+      panic(err)
+    }
+
+    defer resp.Body.Close()
+    fmt.Println("response status:", resp.Status)
+    body, _ := ioutil.ReadAll(resp.Body)
+    fmt.Println("response Body:", string(body))
+    // Now need to pump this to a rest api for EPAgent
+}
